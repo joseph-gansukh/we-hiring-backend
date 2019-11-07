a765b145152c6c53d4605cfee2fe55cf00cbe059
joseph-gansukh <g.sukhbat@gmail.com>
2019-11-07 11:15:55 -0600

    Serving up JSON for Employer

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,2 +1,7 @@
 class ApplicantsController < ApplicationController
+  def index
+    applicants = Applicant.all
+    render json: applicants
+  end
+  
 end
--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,2 +1,12 @@
 class EmployersController < ApplicationController
+  def index
+    employer = Employer.all 
+    render json: employer.to_json( 
+      :include => {
+      :jobs => {:only => [:title, :description, :created_at], 
+      :include => {
+      :applicants => {:only => [:name, :location]}}},
+      }
+    )
+  end
 end
