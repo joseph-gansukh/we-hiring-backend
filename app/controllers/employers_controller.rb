b87c59936fe50fd5f4cec90c69d101ced6b460fe
joseph-gansukh <g.sukhbat@gmail.com>
2019-11-07 12:01:54 -0600

    JSON index for all controllers done

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -2,11 +2,8 @@ class EmployersController < ApplicationController
   def index
     employer = Employer.all 
     render json: employer.to_json( 
-      :include => {
-      :jobs => {:only => [:title, :description, :created_at], 
-      :include => {
-      :applicants => {:only => [:name, :location]}}},
-      }
+      :only => [:id, :name, :location, :field],
+      :include => {:jobs => {:only => [:title, :description, :created_at], :include => {:applicants => {:only => [:name, :location]}}}}
     )
   end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,12 @@
 class JobsController < ApplicationController
+
+    def index
+        job = Job.all
+        render json: job.to_json(
+            :except => [:updated_at, :employer_id],
+            :include => [{:employer => {:only => [:id, :name, :location, :field]}},
+            {:applicants => {:only => [:name, :location]}}]
+        )
+    end
+
 end
