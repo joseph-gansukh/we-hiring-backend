e47b5e5f3089c0433d26b16d894f3ffbbb7f9a8a
joseph-gansukh <g.sukhbat@gmail.com>
2019-11-08 16:49:59 -0600

    updated backend

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -19,5 +19,22 @@ class JobsController < ApplicationController
     )
     end
     
+    def create
+
+      p 'xxxxxxxxxxxxxxxxxxxx'
+      p job_params
+      job = Job.new(job_params)
+      if job.save
+        render json: job
+      else
+        render json: job.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+    
+    def job_params
+      params.require(:job).permit(:title, :description, :employer_id)
+    end
 
 end
