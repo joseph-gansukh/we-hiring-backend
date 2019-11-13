8eff37a21cf765355d05c2ba98c08d6b0e773df2
joseph-gansukh <g.sukhbat@gmail.com>
2019-11-13 17:42:40 -0600

    Job_Applicant json is updated

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,12 +1,12 @@
 class AuthController < ApplicationController
   # skip_before_action :authorized, only: [:create] 
   def create
-    if params[:auth][:userType] == "applicant"
+    if params[:auth][:usertype] == "applicant"
       applicant = Applicant.find_by(name: params[:name])
       #User#authenticate comes from BCrypt
       if applicant && applicant.authenticate(params[:password])
         # encode token comes from ApplicationController
-        token = JWT.encode({ applicant_id: applicant.id, userType: 'applicant' }, 'app_secret', 'HS256')
+        token = JWT.encode({ applicant_id: applicant.id, usertype: 'applicant' }, 'app_secret', 'HS256')
         jsonVariable = applicant.to_json( 
           :only => [:id, :name, :location, :usertype],
           :include => {:jobs => {:only => [:title, :description, :created_at], 
@@ -18,10 +18,10 @@ class AuthController < ApplicationController
         render json: { message: 'Invalid username or password' }, status: :unauthorized
       end
     
-    elsif params[:auth][:userType] == "employer"
+    elsif params[:auth][:usertype] == "employer"
       employer = Employer.find_by(name: params[:name])
       if employer && employer.authenticate(params[:password])
-        token = JWT.encode({employer_id: employer.id, userType: 'employer'}, 'app_secret', 'HS256')
+        token = JWT.encode({employer_id: employer.id, usertype: 'employer'}, 'app_secret', 'HS256')
         jsonVariable = employer.to_json( 
               :only => [:id, :name, :location, :field, :usertype],
               :include => {:jobs => {:only => [:title, :description, :created_at], :include => {:applicants => {:only => [:name, :location]}}}}
@@ -37,7 +37,7 @@ class AuthController < ApplicationController
   def show
     jwt_token = request.headers[:Authorization]
     decode_token = JWT.decode(jwt_token, 'app_secret', true, algorithm: 'HS256')
-    user_type = decode_token[0]['userType']
+    user_type = decode_token[0]['usertype']
     if user_type == 'applicant'
       id = decode_token[0]['id']
       applicant = Applicant.find(id)
--- app/controllers/job_applicants_controller.rb
+++ app/controllers/job_applicants_controller.rb
@@ -2,8 +2,18 @@ class JobApplicantsController < ApplicationController
 
   def create 
     jobApplicant = JobApplicant.create(applicant_id: params[:applicant_id], job_id: params[:job_id])
-
-      render json: {status:"success"}, status: :created
+    applicant = Applicant.find_by(id: params[:applicant_id])
+    jsonVariable = applicant.to_json( 
+          :only => [:id, :name, :location, :usertype],
+          :include => {:jobs => {:only => [:title, :description, :created_at], 
+          :include => {:employer => {:only => [:name, :location]}}}
+          })
+          applicant = JSON.parse jsonVariable
+        render json: {applicant: applicant}
+    # jobs = Job.find_by(applicant: params[:applicant_name])
+      # render json: {applicant: applicant, status:"success", :include => {:jobs => {:only => [:title, :description, :created_at], 
+      # :include => {:employer => {:only => [:name, :location]}}}
+      # }}, status: :created
 
   end
 end
