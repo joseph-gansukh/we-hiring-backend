e1c9cfe2e79987b5ac7669167f4ad7ee3ce07e59
joseph-gansukh <g.sukhbat@gmail.com>
2019-11-13 12:46:04 -0600

    Applicant can apply for jobs and see the jobs applied. Employers can see who applied

--- app/controllers/job_applicants_controller.rb
+++ app/controllers/job_applicants_controller.rb
@@ -1,4 +1,10 @@
 class JobApplicantsController < ApplicationController
 
+  def create 
+    jobApplicant = JobApplicant.create(applicant_id: params[:applicant_id], job_id: params[:job_id])
+
+      render json: {status:"success"}, status: :created
+
+  end
 end
 
